5a7cef23069b26070b5e76d67bcc2500752e761d
Marcio Pancheri <mjpancheri@gmail.com>
2022-02-20 22:02:31 -0300

    read and parse .csv files

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,5 +1,46 @@
 defmodule ReportsGenerator do
-  def hello do
-    :world
+  alias ReportsGenerator.Parser
+
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
+  @options ["foods", "users"]
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
+  end
+
+  def fetch_highest_value(report, option) when option in @options do
+    {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
+  end
+
+  def fetch_highest_value(_report, _option), do: {:error, "Invalid option!"}
+
+  defp sum_values([id, food_name, price], %{"users" => users, "foods" => foods} = report) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+
+    # report
+    # |> Map.put("users", users)
+    # |> Map.put("foods" => foods)
+
+    %{report | "users" => users, "foods" => foods}
+  end
+
+  defp report_acc do
+    foods = Enum.into(@available_foods, %{}, &{&1, 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+
+    %{"users" => users, "foods" => foods}
   end
 end
